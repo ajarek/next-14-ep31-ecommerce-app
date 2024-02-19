c85e8856ec676724a116030fc468d21a170987f0
Ajarek <ajarek@poczta.onet.pl>
2024-02-19 22:12:43 +0100

    added Navbar

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,16 +1,18 @@
-import {getData} from '@/api/fetch'
+import { getData } from '@/api/fetch'
 
+const ProductDetail = async ({ params }: { params: { id: string } }) => {
+  const id = Number(params.id)
+  const { products } = await getData()
+  const product = products.find((product: any) => product.id === id)
+  console.log(product)
 
-const ProductDetail =async ({params}:{params:{id:string}}) => {
-  const id =params.id    
-  const products = await getData()
-  const product=products.find((product:any)=>product.id===id)
   return (
-    <div>ProductDetail {id}
-       <p>{product?.title}</p>
-       <p>{product?.description}</p>
-       <p>{product?.price}</p>
-    </div>
+    <main className='flex min-h-screen flex-col items-center justify-between p-12 max-lg:p-4 max-sm:p-2 '>
+     <h1> ProductDetail {id}</h1>
+      <p>{product?.title}</p>
+      <p>{product?.description}</p>
+      <p>{product?.price}</p>
+    </main>
   )
 }
 
---
+++ components/Navbar.tsx
@@ -0,0 +1,71 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='  navbar bg-base-100 shadow px-24 max-sm:px-4'>
+      <div className='navbar-start'>
+      <div className='dropdown sm:hidden'>
+          <div
+            tabIndex={0}
+            role='button'
+            className='btn btn-ghost btn-circle'
+          >
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              className='h-6 w-6'
+              fill='none'
+              viewBox='0 0 24 24'
+              stroke='currentColor'
+            >
+              <path
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                strokeWidth='2'
+                d='M4 6h16M4 12h16M4 18h7'
+              />
+            </svg>
+          </div>
+          <ul
+            tabIndex={0}
+            className='menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 text-xl gap-6'
+          >
+           <Link  className='hover:underline transition-all ' href='/'>Homepage</Link>
+           <Link  className='hover:underline transition-all ' href='/about'>About</Link>
+           <Link className='relative' href='/cart'>
+            <span text-2xl>🛒</span>
+            <span className='w-6 h-6 badge badge-primary  absolute -top-2 '>0</span>
+           </Link>
+            
+          </ul>
+        </div>
+        <Link href={'/'} className='flex items-center'>
+          <Image
+          src={'/images/logo.png'}
+          alt='logo'
+          width={40}
+          height={40}
+          />
+          <button className='btn btn-ghost text-xl' >E-commerce</button>
+        </Link>
+      </div>
+      <div className='navbar-center'></div>
+      <div className='navbar-end'>
+      <ul
+            
+            className='flex items-center gap-4 max-sm:hidden text-xl '
+          >
+          <Link  className='hover:underline transition-all ' href='/'>Homepage</Link>
+           <Link className='hover:underline transition-all ' href='/about'>About</Link>
+           <Link className='relative' href='/cart'>
+            <span text-2xl>🛒</span>
+            <span className='w-6 h-6 badge badge-primary  absolute -top-2 '>0</span>
+           </Link>
+          </ul>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
